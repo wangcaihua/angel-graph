6895b9630316dbb9b9c10c61b9375b52310d264b
U-TENCENT\fitzwang <fitzwang@fitzwang-NB1.tencent.com>
2020-02-16 22:01:26 +0800

    add ref hash map

---
+++ graph-core/src/main/scala/com/tencent/angel/graph/core/data/GData.scala
@@ -0,0 +1,53 @@
+package com.tencent.angel.graph.core.data
+
+import com.tencent.angel.common.Serialize
+import com.tencent.angel.graph.utils.{ReflectUtils, SerDe}
+import io.netty.buffer.ByteBuf
+
+import scala.reflect.runtime.universe._
+
+
+trait GData extends Serialize {
+  def serialize(byteBuf: ByteBuf): Unit = {
+    SerDe.serialize(this, GData.getFields(this), byteBuf)
+  }
+
+  def deserialize(byteBuf: ByteBuf): Unit = {
+    SerDe.deserialize(this, GData.getFields(this), byteBuf)
+  }
+
+  def bufferLen(): Int = {
+    SerDe.bufferLen(this, GData.getFields(this))
+  }
+}
+
+object GData {
+  private val fields = new scala.collection.mutable.HashMap[String, List[TermSymbol]]()
+  private val types = new scala.collection.mutable.HashMap[String, Type]()
+
+  def getType(node: GData): Type = types.synchronized {
+    val name = node.getClass.getCanonicalName
+
+    if (types.contains(name)) {
+      types(name)
+    } else {
+      val tpe = ReflectUtils.getType(node)
+      types.put(name, tpe)
+
+      tpe
+    }
+  }
+
+  def getFields(node: GData): List[TermSymbol] = fields.synchronized {
+    val name = node.getClass.getCanonicalName
+
+    if (fields.contains(name)) {
+      fields(name)
+    } else {
+      val field = ReflectUtils.getFields(ReflectUtils.getType(node))
+      fields.put(name, field)
+
+      field
+    }
+  }
+}
---
+++ graph-core/src/main/scala/com/tencent/angel/graph/core/data/RefHashMapManager.scala
@@ -0,0 +1,94 @@
+package com.tencent.angel.graph.core.data
+
+import java.util
+
+import scala.reflect.ClassTag
+
+
+class RefHashMapManager(ref: AnyRef, matrixId: Int, partitionId: Int) {
+  private val clz = ref.getClass
+
+  private val maskField = {
+    val field = clz.getDeclaredField("mask")
+    field.setAccessible(true)
+    field
+  }
+
+  private val nField = {
+    val field = clz.getDeclaredField("n")
+    field.setAccessible(true)
+    field
+  }
+
+  private val containsNullKeyField = {
+    val field = clz.getDeclaredField("containsNullKey")
+    field.setAccessible(true)
+    field
+  }
+
+  private val keyField = {
+    val field = clz.getDeclaredField("key")
+    field.setAccessible(true)
+    field
+  }
+
+  private val valueMap: util.HashMap[String, Any] = new util.HashMap[String, Any]()
+
+  val mask = maskField.get(ref).asInstanceOf[Int]
+
+  val n = nField.get(ref).asInstanceOf[Int]
+
+  val containsNullKey = containsNullKeyField.get(ref).asInstanceOf[Boolean]
+
+  val innerSize: Int = keyField.get(ref).asInstanceOf[Array[_]].length
+
+  def addValues[V](name: String, values: Array[V]): this.type = valueMap.synchronized {
+    assert(innerSize == values.length)
+    valueMap.put(name, values)
+
+    this
+  }
+
+  def addValues[V: ClassTag](name: String): this.type = valueMap.synchronized {
+    val values = new Array[V](innerSize)
+    valueMap.put(name, values)
+
+    this
+  }
+
+  def removeArray(name: String): Unit = valueMap.synchronized {
+    if (valueMap.containsKey(name)) {
+      valueMap.remove(name)
+    }
+  }
+
+  def getIntKeyHashMap[V](name: String): IntKeyRefHashMap[V] = {
+    if (!valueMap.containsKey(name)) {
+      throw new Exception(s"cannot find $name in valueMap !")
+    }
+
+    assert(mask == maskField.get(ref).asInstanceOf[Int] &&
+      n == nField.get(ref).asInstanceOf[Int] &&
+      containsNullKey == containsNullKeyField.get(ref).asInstanceOf[Boolean] &&
+      innerSize == keyField.get(ref).asInstanceOf[Array[_]].length)
+
+    val keys = keyField.get(ref).asInstanceOf[Array[Int]]
+    val values = valueMap.get(name).asInstanceOf[Array[V]]
+    new IntKeyRefHashMap[V](mask, n, containsNullKey, keys, values)
+  }
+
+  def getLongKeyHashMap[V](name: String): LongKeyRefHashMap[V] = {
+    if (!valueMap.containsKey(name)) {
+      throw new Exception(s"cannot find $name in valueMap !")
+    }
+
+    assert(mask == maskField.get(ref).asInstanceOf[Int] &&
+      n == nField.get(ref).asInstanceOf[Int] &&
+      containsNullKey == containsNullKeyField.get(ref).asInstanceOf[Boolean] &&
+      innerSize == keyField.get(ref).asInstanceOf[Array[_]].length)
+
+    val keys = keyField.get(ref).asInstanceOf[Array[Long]]
+    val values = valueMap.get(name).asInstanceOf[Array[V]]
+    new LongKeyRefHashMap[V](mask, n, containsNullKey, keys, values)
+  }
+}
